49921df1552cf5426b2868033a8304faca41616d
DHRUVRastogi-123 <57324344+DHRUVRastogi-123@users.noreply.github.com>
2023-11-22 22:30:50 +0530

    Update Canvas.jsx

--- frontend/src/Components/Canvas/Canvas.jsx
+++ frontend/src/Components/Canvas/Canvas.jsx
@@ -42,7 +42,7 @@ const Canvas = (props) => {
   const handleSaveCanvas = async () => {
     // code for saving the canvas to the mongoDB
     try {
-      const response = await fetch("http://localhost:5000/canvas", {
+      const response = await fetch("https://taskify-zi22.onrender.com/canvas", {
         method: "POST",
         credentials: "include",
         headers: {
