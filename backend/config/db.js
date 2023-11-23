c5561a07512b4da5b96c723a2e2caa27f43f0411
DHRUVRastogi-123 <57324344+DHRUVRastogi-123@users.noreply.github.com>
2023-11-23 13:24:45 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     let conn;
 
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/taskify", { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect("mongodb+srv://dr0458571:zzh6Y5mHRKRIqqxI@chakracoders.vfsqzg5.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
         conn = mongoose.connection;
         console.log(`MongoDB Connected: ${conn.host}`);
     } catch (error) {
